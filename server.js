b77892fdf9af6f23fb565c1d573f00e6ae10b085
betty <betzuyu@gmail.com>
2023-11-20 10:47:23 +0700

    express-crud

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const userRouter = require("./router/users");
+const connectDB = require("./config/db");
+
+const port = 3000;
+
+const cors = require("cors");
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use(userRouter);
+
+connectDB();
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
